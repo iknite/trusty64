5c442f76fb03af61fe8591a148c9bc67f7ac1e34
iknite <enrique@iknite.com>
2015-01-03 16:57:06 +0100

    refactor to isolate virtualbox needs

---
+++ install-virtualbox.sh
@@ -0,0 +1,24 @@
+
+# Setup sudo to allow no-password sudo for "admin"
+groupadd -r admin
+usermod -a -G admin vagrant
+
+# vagrant user needs sudo without ask password, yikes
+sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
+sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
+
+
+# sudo install requiered dependencies
+sudo apt-get install linux-headers-generic build-essential dkms
+
+# install virtualbox guest additions
+# mount iso
+mkdir /tmp/isomount
+mount -t iso9660 -o loop /tmp/VBoxGuestAdditions.iso /tmp/isomount
+
+# Install the drivers
+/tmp/isomount/VBoxLinuxAdditions.run
+
+# Cleanup
+umount /tmp/isomount
+rm -rf /tmp/isomount /tmp/VBoxGuestAdditions.iso
